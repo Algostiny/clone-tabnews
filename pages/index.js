5afd280b041f59e5ea432d8fdc339caf926a90aa
Augusto <67249509+Algostiny@users.noreply.github.com>
2023-07-07 00:53:34 +0000

    Adiciona estilo e altera o h1 no index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu te amo, Keity!</h1>
+    return <h1 style={{width:100+'vw', textAlign:'center', paddingTop:40+'vh'}}>Eu te amo, Keity!</h1>
 }
 
 export default Home;
